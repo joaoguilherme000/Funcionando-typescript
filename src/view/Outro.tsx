9f741ef3a3c79328ddbc96d3541d8e03b04b8a0f
joaoguilherme <joaoguilhermehig@gmail.com>
2023-10-10 18:26:58 -0300

    2 feito

--- src/view/Outro.tsx
+++ src/view/Outro.tsx
@@ -1,19 +1,17 @@
-import { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, Text, SafeAreaView,TouchableOpacity, Image} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Text, SafeAreaView, TouchableOpacity, Image, Modal } from 'react-native';
 import { CameraType, Camera } from 'expo-camera';
 import Styles from "../view/Styles";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-
 export default function Outro() {
     const camRef = useRef<Camera>(null);
-    
     const insets = useSafeAreaInsets();
 
     const [type, setType] = useState<CameraType>(Camera.Constants.Type.back);
     const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-
-    const [uriDaImagem, setUriDaImagem] = useState<string | null>(null); // Novo estado para a URI da imagem
+    const [open, setOpen] = useState(false); // Estado para controlar a abertura do modal
+    const [uriDaImagem, setUriDaImagem] = useState<string | null>(null);
 
     useEffect(() => {
         (async () => {
@@ -24,10 +22,11 @@ export default function Outro() {
 
     const tirarFoto = async () => {
         if (camRef.current) {
-          const photo = await camRef.current.takePictureAsync();
-          setUriDaImagem(photo.uri);
+            const photo = await camRef.current.takePictureAsync();
+            setUriDaImagem(photo.uri);
+            setOpen(true); // Abre o modal quando a foto é tirada
         }
-      };
+    };
 
     if (hasPermission === null) {
         return <View />;
@@ -39,16 +38,34 @@ export default function Outro() {
     return (
         <SafeAreaView style={styles.container}>
             <Camera
-                style={{ width: '100%', marginTop: insets.top, height: "89%",  }}
+                style={{ width: '100%', marginTop: insets.top, height: "89%" }}
                 type={type}
                 ref={camRef}
             >
-                <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}/>
+                <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }} />
             </Camera>
-            <TouchableOpacity onPress={tirarFoto}>
-                <Text>Tirar Foto</Text>
-            </TouchableOpacity>
-            {uriDaImagem && <Image source={{ uri: uriDaImagem }} style={styles.imagem} />}
+            <View style={{ justifyContent: 'center', width: "100%", height: 25, flexDirection: "row", gap: 50 }}>
+                <TouchableOpacity onPress={tirarFoto}>
+                    <Text>Tirar Foto</Text>
+                </TouchableOpacity>
+            </View>
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={open}
+                style={styles.modal}
+            >
+                {uriDaImagem && <Image
+                    source={{ uri: uriDaImagem }}
+                    style={styles.imagem}
+                />}
+                <TouchableOpacity onPress={() => setOpen(false)}>
+                    <Text>Fechar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>Salvar na galeria</Text>
+                </TouchableOpacity>
+            </Modal>
         </SafeAreaView>
     );
 }
@@ -60,7 +77,12 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     imagem: {
-        width: 200, // Defina o tamanho desejado para a imagem
-        height: 200, // Defina o tamanho desejado para a imagem
+        width: 200,
+        height: 200,
     },
+    modal: {
+        alignItems: 'center',
+        justifyContent:'center',
+        flex: 1,
+    }
 });
